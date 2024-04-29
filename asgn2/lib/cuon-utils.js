b5224b8b9ab800b818a61ddfaf12411c7beaaead
abnferre <166665548+abnferre@users.noreply.github.com>
2024-04-28 23:16:45 -0700

    Create cuon-utils.js

---
+++ asgn2/lib/cuon-utils.js
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>BlockyAnimal</title>
+    
+  </head>
+
+  <body onload="main()">
+    <p>Ari Ferreros</p>
+    <p>abnferre@ucsc.edu</p>
+    <p>Notes: added 3 animations, joint from feet to body to head</p>
+    <p>need to wait a bit or turn off then on the animation when changing speed</p>
+    <p>shift-click to cycle through animations</p>
+    <div>
+    <canvas id="webgl" width="400" height="400">
+    Please use a browser that supports "canvas"
+    </canvas>
+    
+</div>
+    <p>
+    <p id= "numdot"> XXX</p>
+
+    <button type= "button" id= "poseRESET">RESET POSE</button>
+      <div>
+        <label for="angleSlide">Camera Angle X</label>
+        <input type="range" id="angleSlide" class= "slider" min="0" max="360" value="20" />
+      </div>
+      <div>
+        <div>
+          <label for="angleSlideY">Camera Angle Y</label>
+          <input type="range" id="angleSlideY" class= "slider" min="0" max="360" value="5" />
+        </div>
+        <div>
+      <div>
+          <label for="headSlide">Rotate Head</label>
+          <input type="range" id="headSlide" class= "slider" min="-30" max="15" value="0" />
+        </div>
+        <div>
+          <label for="armSlide">Rotate Left arm</label>
+          <input type="range" id="armSlide" class= "slider" min="30" max="160" value="30" />
+          
+
+        </div>
+        <div>
+          <label for="armSlideR">Rotate Right arm</label>
+          <input type="range" id="armSlideR" class= "slider" min="30" max="160" value="30" />
+        </div>
+        <div>
+          <label for="bodySlide">Rotate Body</label>
+          <input type="range" id="bodySlide" class= "slider" min="-25" max="25" value="0" />
+        </div>
+        <!--
+        <div>
+            <label for="feetSlide">Rotate Feet</label>
+            <input type="range" id="feetSlide" class= "slider" min="-25" max="25" value="0" />
+        </div> -->
+        <h3>Animations</h3>
+          <div>
+            <label for="Animations">Wave Animation</label>
+            <button type= "button" id= "armLAnimationON">ON</button>
+            <button type= "button" id= "armLAnimationOFF">OFF</button>
+          </div>
+          <div>
+            <label for="Animations">Happy Animation</label>
+            <button type= "button" id= "happyAnimationON">ON</button>
+            <button type= "button" id= "happyAnimationOFF">OFF</button>
+            <label for="happyAnimationSpeed">Speed</label>
+            <input type="range" id="happyAnimationSpeed" class= "slider" min="1" max="30" value="5" />
+          </div>
+          <div>
+            <label for="Animations">Run Animation</label>
+            <button type= "button" id= "runAnimationON">ON</button>
+            <button type= "button" id= "runAnimationOFF">OFF</button>
+            <label for="runAnimationSpeed">Speed</label>
+            <input type="range" id="runAnimationSpeed" class= "slider" min="1" max="30" value="20" />
+          </div>
+
+        </div>
+        
+      
+      
+      <script src="../lib/webgl-utils.js"></script>
+    <script src="../lib/webgl-debug.js"></script>
+    <script src="../lib/cuon-utils.js"></script>
+    <script src="../lib/cuon-matrix-cse160.js"></script>
+    <script src="Point.js"></script>
+    <script src="BlockyAnimal.js"></script>
+    <script src="Triangle.js"></script>
+    <script src="Cube.js"></script>
+    <script src="Circle.js"></script>
+   
+  </body>
+</html>
